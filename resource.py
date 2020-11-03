ff3d53e76224af4ddcea3d42e5e0c5a3c5a58abb
huuhau <nguyenhuuhau1061995@gmail.com>
2020-11-03 21:32:05 +0700

    add

--- resource.py
+++ resource.py
@@ -1,17 +1,16 @@
-# from flask import Flask
-# from flask_restful import Api
+from flask import Flask
+from flask_restful import Api
 
-print('aaaaaaa')
 
-# from connectDB.ApartmentsAPI import ApartmentsCollectionAPI
+from connectDB.ApartmentsAPI import ApartmentsCollectionAPI
 
-# app = Flask(__name__)
-# api = Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-# api.add_resource(ApartmentsCollectionAPI, '/apartments', methods = ['GET', 'POST'])
+api.add_resource(ApartmentsCollectionAPI, '/apartments', methods = ['GET', 'POST'])
 
-# if __name__ == '__main__':
-# 	try:
-# 		app.run(host='127.0.0.1', port=3001, debug=True)
-# 	except Exception as exp:
-# 		print (exp)
+if __name__ == '__main__':
+	try:
+		app.run(host='127.0.0.1', port=3001, debug=True)
+	except Exception as exp:
+		print (exp)
